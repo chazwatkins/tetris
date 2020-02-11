84363f94eae0f0c0d818215cd79f5afd0bd98afb
Chaz Watkins <chazwatkins@live.com>
2020-02-11 00:45:14 -0600

    add Tetris.Points

---
+++ lib/tetris/points.ex
@@ -0,0 +1,49 @@
+defmodule Tetris.Points do
+  def translate(points, {x, y}) do
+    Enum.map(points, fn {dx, dy} -> {dx + x, dy + y} end)
+  end
+
+  def transpose(points), do: Enum.map(points, fn {x, y} -> {y, x} end)
+
+  def mirror(points), do: Enum.map(points, fn {x, y} -> {5 - x, y} end)
+  def mirror(points, false), do: points
+  def mirror(points, true), do: mirror(points)
+
+  def flip(points), do: Enum.map(points, fn {x, y} -> {x, 5 - y} end)
+
+  def rotate_90(points) do
+    points
+    |> transpose()
+    |> mirror()
+  end
+
+  def rotate(points, 0), do: points
+
+  def rotate(points, degrees) do
+    points
+    |> rotate_90()
+    |> rotate(degrees - 90)
+  end
+
+  def to_string(points) do
+    map =
+      points
+      |> Enum.map(fn key -> {key, "◼"} end)
+      |> Map.new()
+
+    for x <- 1..4, y <- 1..4 do
+      Map.get(map, {x, y}, "◻")
+    end
+    |> Stream.chunk_every(4)
+    |> Stream.map(&Enum.join/1)
+    |> Enum.join("\n")
+  end
+
+  def print(points) do
+    points
+    |> __MODULE__.to_string()
+    |> IO.puts()
+
+    points
+  end
+end
