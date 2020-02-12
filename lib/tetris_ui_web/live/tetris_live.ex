fbeb0a8735214a452c1e186c90cefb02e077da07
Chaz Watkins <chazwatkins@live.com>
2020-02-11 20:58:57 -0600

    render blocks

--- lib/tetris_ui_web/live/tetris_live.ex
+++ lib/tetris_ui_web/live/tetris_live.ex
@@ -1,18 +1,119 @@
 defmodule TetrisUIWeb.TetrisLive do
   use Phoenix.LiveView
+  import Phoenix.HTML, only: [raw: 1]
+
+  @box_width 20
+  @box_height 20
 
   def mount(_params, _session, socket) do
-    tetromino =
-      Tetris.Brick.new_random()
-      |> Tetris.Brick.to_string()
-
-    {:ok, assign(socket, tetromino: tetromino)}
+    {:ok, new_game(socket)}
   end
 
-  @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~L"""
-    <pre><%= @tetromino %></pre>
+    <div>
+      <%= raw svg_head() %>
+      <%= raw boxes(@tetromino) %>
+      <%= raw svg_foot() %>
+    </div>
     """
   end
+
+  defp new_game(socket) do
+    assign(
+      socket,
+      state: :playing,
+      score: 0
+    )
+    |> new_block()
+    |> show()
+  end
+
+  def new_block(socket) do
+    brick =
+      Tetris.Brick.new_random()
+      |> Map.put(:location, {3, 1})
+
+    assign(socket, brick: brick)
+  end
+
+  def show(socket) do
+    brick = socket.assigns.brick
+
+    points =
+      brick
+      |> Tetris.Brick.prepare()
+      |> Tetris.Points.with_color(color(brick))
+
+    assign(socket, tetromino: points)
+  end
+
+  def svg_head() do
+    """
+    <svg
+    version="1.0"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="200" height="400"
+    viewBox="0 0 200 400"
+    xml:space="preserve">
+    """
+  end
+
+  def svg_foot(), do: "</svg>"
+
+  def boxes(points_with_colors) do
+    points_with_colors
+    |> Stream.map(fn {x, y, color} ->
+      box({x, y}, color)
+    end)
+    |> Enum.join("\n")
+  end
+
+  def box(point, color) do
+    box_color = shades(color)
+
+       #{triangle(point, box_color.dark)}
+
+    """
+    #{square(point, box_color.light)}
+    """
+  end
+
+  def square(point, shade) do
+    {x, y} = to_pixels(point)
+
+    """
+    <rect
+      x="#{x + 1}" y="#{y + 1}"
+      style="fill:##{shade};"
+      width="#{@box_width - 2}" height="#{@box_height - 2}" />
+    """
+  end
+
+  def triangle(point, shade) do
+    {x, y} = to_pixels(point)
+    {w, h} = {@box_width, @box_height}
+
+    """
+    <polyline
+      style="fill:##{shade};"
+      points="#{x + 1},#{y + 1} #{x + w},#{y + 1} #{x + w},#{y + h} />
+    """
+  end
+
+  def to_pixels({x, y}), do: {x * @box_width, y * @box_height}
+
+  defp shades(:red), do: %{light: "DB7160", dark: "AB574B"}
+  defp shades(:blue), do: %{light: "83C1C8", dark: "66969C"}
+  defp shades(:green), do: %{light: "8BBF57", dark: "769359"}
+  defp shades(:orange), do: %{light: "CB8E4E", dark: "AC7842"}
+  defp shades(:grey), do: %{light: "A1A09E", dark: "7F7F7E"}
+
+  defp color(%{name: :t}), do: :red
+  defp color(%{name: :i}), do: :blue
+  defp color(%{name: :l}), do: :green
+  defp color(%{name: :o}), do: :orange
+  defp color(%{name: :z}), do: :grey
 end
